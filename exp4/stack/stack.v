d8e731b340d533347b10f1c33ad8ccfdd20e802c
Ali Hatami <57178883+hatam008@users.noreply.github.com>
2021-04-07 16:15:59 +0430

    exp5 semi-finel
    waiting for TA respond that one assumption is right or not. it works though

---
+++ exp4/stack/stack.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module stack(
+    input clk,
+    input rstN,
+    input [3:0] data_in,
+    input push,
+    input pop,
+    output [3:0] data_out,
+    output full,
+    output empty
+    );
+    // point to the empty index of stack
+    reg [3:0]emptyPos = 0;
+    // stack with depth 8 and width 4
+    reg [3:0]stack[0:7];
+    // out register
+    reg [3:0] out = 0;
+    
+    // assign empty and full signals
+    assign full = (emptyPos == 8);
+    assign empty = (emptyPos == 0);
+    assign data_out = out;
+    
+    // asynchronized reset stack signal
+    always @ (rstN) begin
+        emptyPos <= 0;
+        out <= 0;
+    end
+    
+    always @ (posedge clk) begin
+        if (pop && push) begin
+            if (empty) begin // If Pop and Push together but pop can't be done
+                stack[emptyPos] = data_in;
+                emptyPos = emptyPos + 1;
+            end else begin // push and pop together
+                out = stack[emptyPos - 1];
+                stack[emptyPos - 1] = data_in;
+            end
+        end else if (pop && ~empty) begin // if only pop and pop is valid
+            emptyPos = emptyPos - 1;
+            out = stack[emptyPos];
+        end else if (push && ~full) begin // if only push and push is valid
+            stack[emptyPos] = data_in;
+            emptyPos = emptyPos + 1;      
+        end
+    end
+endmodule
---
+++ exp4/stack/stack_tb.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+// Stack Test Bench
+
+module stack_tb();
+    reg clk = 0, pop = 0, push = 0, rstN = 0;
+    reg [3:0]data_in = 0;
+    wire [3:0] data_out;
+    wire full, empty;
+    stack uut(
+        .clk (clk),.rstN (rstN),.data_in (data_in),.push (push),.pop (pop),
+        .data_out (data_out),.full (full),.empty (empty)
+    );
+    
+    // Generating Clock Signal
+    initial begin 
+        forever
+            #5 clk = ~clk;
+    end
+    integer i = 0;
+    initial begin 
+        #10;
+        // push 5 data every clock
+        push = 1;
+        for (i = 0; i<5; i = i + 1) begin data_in = $random; #10; end
+        // pop last data
+        push = 0;
+        pop = 1; #10;
+        // push pop in one clock
+        data_in = 4;
+        push = 1; #10;
+        // pop again result must be 4
+        push = 0; #10;
+        // push 6 data (5 valid push / one invelid push)
+        pop = 0; push =1;
+        for (i = 0; i<6; i = i + 1) begin data_in = $random; #10; end
+        // pop last 3 data (it should be the 5th data in last push part)
+        push = 0; 
+        pop =1; #30;
+        // reset the stack
+        pop =0; rstN = 1; #5 rstN = 0; #5;
+        // 2 invalid pop
+        pop = 1; #20;
+        // push a new data with pop on and pop it
+        data_in = 14; push = 1; #10; // push data with push and pop on but pop is invalid
+        push = 0; #10; //pop the last entry
+        // change in data with push off
+        pop = 0;
+        for (i = 0; i<3; i = i + 1) begin data_in = $random; #10; end // change data in with push off
+        #5 $finish;
+    end
+endmodule
