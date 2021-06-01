2368a65717d5e1eb70d16d44c857840fa69c0b58
Ali Hatami <a.hatam008@gmail.com>
2021-06-02 02:29:53 +0430

    pushc multi completed

--- exp10/processor/processor_multicycle.v
+++ exp10/processor/processor_multicycle.v
@@ -19,6 +19,7 @@ input wire [7:0] ram_data_in, stack_data_in;
 output reg [7:0] ram_address, ram_data_out, stack_data_out;
 output reg ram_readWriteN, stack_push, stack_pop;
 
+localparam INITS =      4'd14;
 localparam FETCH1 =     4'd12;
 localparam FETCH2 =     4'd13;
 localparam PUSHC =      4'd0; // also pushc   opcode
@@ -34,31 +35,81 @@ localparam ADD_SECOND = 4'd10;
 localparam SUB_FIRST =  4'd7; // also sub     opcode
 localparam SUB_SECOND = 4'd11;
 
-reg [3:0] current_state = 4'd12;
+reg [3:0] current_state = 4'd14;
 reg [3:0] opcode;
+reg [7:0] oprand;
 
 reg z_flag, s_flag;
+
+reg [7:0] pc;  // PROGRAM COUNTER
 
 always @(posedge clk) begin
     if (~resetN) begin
         // TODO: RESET PROCEDURE
+            // program counter set to zero
     end else if (haltN) begin
         case (current_state)
-            FETCH1: begin // opcode be fetched
-                
+            INITS: begin // program counter will be set, read signal will be set
+                pc <= 16'b0;
+                ram_readWriteN <= 1b'1;
+                stack_pop <= 1'b0;
+                stack_push <= 1'b0;   
+                ram_address <=  1'b0;
+                ram_readWriteN <= 1b'1;
+                current_state <= FETCH1; 
+            end
+            
+            FETCH1: begin // opcode be fetched, the address must be updated in the last state/clock
+                opcode <= ram_data_in[3:0];
+                ram_readWriteN <= 1b'1;
+                if (ram_data_in < 4 ) begin // if opcode is pushc/push/pop
+                    address <= pc + 1;
+                    pc <= pc + 2;
+                end else begin
+                    pc <= pc + 1;
+                end
+
+                current_state <= FETCH2;
             end 
 
             FETCH2: begin // oprand will be fetched: this state is important even if
             // no oprand is fetched because it  will decide where we go next
             // oprand will be fetched if opcode need it and the oprand will be loaded where it is nedded
             // based on the opcode
-                
-                //TODO: push and pop sould goes off
+                oprand <= ram_data_in;
+                stack_pop <= 1'b0;
+                // make stack push signal ready. 
+                if (opcode == PUSHC) stack_push <= 1'b1;
+                else stack_push <= 1'b1;
+
+                case (opcode) begin
+                    PUSHC: begin    // data to push prepared here
+                        // prepare data to be pushed
+                        stack_push <= 1'b1;
+                        stack_data_out <= ram_data_in;
+                        current_state = PUSHC;
+                        
+                        // prepare to fetch next instruction
+                        ram_address <= pc;
+                        ram_readWriteN <= 1b'1;
+                    end
+                end
             end
 
             PUSHC: begin // the push signal is one from last state
             // this state do the job, change the signal, and fetch the next opcode
+                stack_push <= 1'b0;
                 
+                // fetch1 procedures
+                opcode <= ram_data_in[3:0];
+                ram_readWriteN <= 1b'1;
+                if (ram_data_in < 4 ) begin // if opcode is pushc/push/pop
+                    address <= pc + 1;
+                    pc <= pc + 2;
+                end else begin
+                    pc <= pc + 1;
+                end
+                current_state <= FETCH2;
             end
 
             PUSHM_READ: begin   // read signal is one from last state - it will store the data
